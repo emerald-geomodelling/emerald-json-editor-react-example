87f0563d7f7797675e4f1f8916d5e40153e2e4b5
martineho <martineolsenn@hotmail.com>
2024-01-03 14:59:58 -0500

    Install npm emerald-json-editor-react

--- example-app/src/app/ui/JsonForm.js
+++ example-app/src/app/ui/JsonForm.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { JsonEditorWrapper } from "emerald-json-editor-react";
 
 const JsonForm = () => {
   return (
     <div className="border border-gray-200 text-gray-700 p-5 rounded-lg">
+      <JsonEditorWrapper />
       Form here
     </div>
   );
