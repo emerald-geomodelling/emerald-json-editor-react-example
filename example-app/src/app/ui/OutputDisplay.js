4b52783adecfa6b49aafc9dbf2ac6e115d07c706
martineho <martineolsenn@hotmail.com>
2024-01-03 14:41:16 -0500

    Update OutputDisplay.js

--- example-app/src/app/ui/OutputDisplay.js
+++ example-app/src/app/ui/OutputDisplay.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Source_Code_Pro } from "next/font/google";
+
+const font = Source_Code_Pro({ subsets: ["latin"] });
 
 const OutputDisplay = () => {
   return (
-    <div className="bg-gray-700 text-gray-300 p-5 rounded-lg">
-      OutputDisplay
+    <div
+      className={`bg-gray-700 text-gray-300 p-5 rounded-lg ${font.className}`}
+    >
+      Output Display
     </div>
   );
 };
