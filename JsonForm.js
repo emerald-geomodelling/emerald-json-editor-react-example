bab770911107b2d509a3634cff1d3a72a24de51b
martineho <martineolsenn@hotmail.com>
2024-01-04 13:05:56 -0500

    delete next js app

--- JsonForm.js
+++ JsonForm.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { JsonEditorWrapper } from "emerald-json-editor-react";
+import dynamic from "next/dynamic";
+
+const DynamicJsonEditor = dynamic(
+  () => import("emerald-json-editor-react").then((mod) => mod.default),
+  { ssr: false }
+);
 
 const JsonForm = () => {
   return (
     <div className="border border-gray-200 text-gray-700 p-5 rounded-lg">
-      <JsonEditorWrapper />
+      <DynamicJsonEditor />
       Form here
     </div>
   );
